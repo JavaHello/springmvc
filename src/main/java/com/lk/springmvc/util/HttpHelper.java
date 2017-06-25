5229fafd6b5f94ec525a75ab03e2c1312be51f92
luokai <272389184@qq.com>
2017-06-25 18:32:29 +0800

    Test vim edit

---
+++ src/main/java/com/lk/springmvc/util/HttpHelper.java
@@ -0,0 +1,7 @@
+package com.lk.springmvc.util;
+
+/**
+ * Created by luokai on 17-6-25.
+ */
+public class HttpHelper {
+}
---
+++ src/test/java/com/lk/springmvc/util/HttpHelperTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by luokai on 17-6-25.
+ */
+public class HttpHelperTest {
+
+}
